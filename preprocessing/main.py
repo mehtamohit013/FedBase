237ce38eca4ff50a58799dea7e6246404c10a983
mehtamohit <mehtamohit013@gmail.com>
2022-04-12 16:58:29 +0530

    Moved Preprocessing and added comments to main.py

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -7,8 +7,10 @@ import tqdm
 
 from process_gps import GEngine
 from process_lidar import LEngine
-from process_OSM import HOME, OSMEngine
+from process_OSM import OSMEngine
 
+
+# Data Paths
 HOME = os.environ['HOME']
 lpath = os.path.join(HOME,'webots_code','data','final','lidar_samples')
 gpath = os.path.join(HOME,'webots_code','data','final','samples')
@@ -16,11 +18,14 @@ tpath = os.path.join(HOME,'webots_code','data','final','tracking')
 opath = os.path.join(HOME,'webots_code','data','final','OSM')
 matpath = os.path.join(HOME,'webots_code','data','final','MAT')
 gspath = os.path.join(HOME,'webots_code','data','final','gps.pkl')
-
 os.makedirs(opath,exist_ok=True)
 
+
+# Simulation Timestep
 timestep = 0.128
 
+
+# Buggy, Not using currently
 use_site = 1
 site_origin = np.array([
     [[-170,5,-113],
@@ -33,31 +38,35 @@ site_origin = np.array([
 origins = site_origin[use_site]
 n_sites = origins.shape[0]
 
+
+# Parameters
 steps = np.array([1.0, 0.5, 1.0]) #Step size (x,y,z) #For Quantization
 cube = [-1, 1, -5, 5, -3, 3] # cube to remove, centered at lidar, before origin shift
 
+
+# Process Pool: For multiprocessing
 p = mp.Pool()
 
+# Lidar Preprocessing
 print(f'Starting Lidar preprocessing'+'.'*10)
 lidar = LEngine(lpath,cube,steps,origins)
-
 for _ in tqdm.tqdm(p.imap_unordered(lidar,os.listdir(lpath)),total=len(os.listdir(lpath))):
     pass
 print(f'Lidar Preprocessing ended')
 
+# GPS Preprocessing
 print(f'Starting GPS preprocessing'+'.'*10)
 gps = GEngine(gpath,matpath)
 gps(gspath)
 print('GPS preprocessing finished')
 
+# OSM Preprocessing
 print(f'Starting OSM preprocessing'+'.'*10)
 osm = OSMEngine(gspath,tpath,opath,timestep)
 gps_pd = pd.read_pickle(gspath)
-
-start = time.time()
 for _ in tqdm.tqdm(p.imap_unordered(osm,gps_pd.index.values),total=len(gps_pd.index.values)):
     pass
-
 print(f'OSM preprocessing ended')
+
 
 print('*'*10+'Preprocessing Ended'+'*'*10)
---
+++ to-do.md
@@ -0,0 +1,2 @@
+# Arguments
+Change the data path in monitor\_vehcile.py and ray\_power.py. Can replace them by a global config file.
